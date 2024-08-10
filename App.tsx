11caa1752dab210b4a3e0069825c12bc84c27021
fmacedosantos <felipemacedo986@gmail.com>
2024-08-09 22:05:00 -0300

    Adicionando as caixas

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function CaixaRed(){
+  return <View style={styles.caixaRed}></View>
+}
+
+function CaixaBlue(){
+  return <View style={styles.caixaBlue}></View>
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <CaixaRed></CaixaRed>
+      <CaixaBlue></CaixaBlue>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  caixaRed: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'red'
+  },
+  caixaBlue: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'blue'
   },
 });
